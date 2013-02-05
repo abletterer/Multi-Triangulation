1a4b0ab94603297f2e610754113f81fc639b4db8
bletterer <bletterer@pcc311.psi.ad.unistra.fr>
2013-02-05 09:34:46 +0100

    Ajout des fichiers/dossiers de base de l'application

---
+++ include/VDPMesh_App.h
@@ -0,0 +1,62 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2011, IGG Team, LSIIT, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+#ifndef _VDPMesh_App_
+#define _VDPMesh_App_
+#include "Utils/Qt/qtSimple.h"
+#include "ui_VDPMesh_App.h"
+#include "Utils/Qt/qtui.h"
+
+using namespace CGoGN;
+
+class VDPMesh_App: public Utils::QT::SimpleQT
+{
+	Q_OBJECT
+public:
+
+	VDPMesh_App() {}
+
+	~VDPMesh_App() {}
+
+	void cb_redraw();
+
+	void cb_initGL();
+
+	//void cb_mousePress(int button, int x, int y);
+
+	//void cb_mouseRelease(int button, int x, int y);
+
+	//void cb_mouseClick(int button, int x, int y);
+
+	//void cb_mouseMove(int buttons, int x, int y);
+
+	//void cb_wheelEvent(int delta, int x, int y);
+
+	//void cb_keyPress(int code);
+
+	//void cb_keyRelease(int code);
+
+public slots:
+};
+
+#endif
---
+++ src/VDPMesh_App.cpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2011, IGG Team, LSIIT, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.u-strasbg.fr/                                         *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#include "VDPMesh_App.h"
+
+void VDPMesh_App::cb_initGL()
+{}
+
+void VDPMesh_App::cb_redraw()
+{}
+
+int main(int argc, char **argv)
+{
+
+	QApplication app(argc, argv);
+	VDPMesh_App sqt;
+	sqt.setWindowTitle("VDPMesh_App");
+
+	Utils::QT::uiDockInterface dock;
+	sqt.setDock(&dock);
+
+	sqt.show();
+
+	return app.exec();
+}
