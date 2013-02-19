775dfcb051d9114a594a0e65ed4d2236e10c5b74
Arnaud Bletterer <arnaud.bletterer@gmail.com>
2013-02-19 15:33:14 +0100

    []

--- include/VDPMesh.h
+++ include/VDPMesh.h
@@ -1,71 +1,110 @@
-#ifndef __VDPMESH_H__
-#define __VDPMESH_H__
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2012, IGG Team, LSIIT, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
 
-#include "Topology/map/embeddedMap2.h"
+#ifndef __PMESH__
+#define __PMESH__
+
+#include "VSplit.h"
+
 #include "Algo/Decimation/selector.h"
 #include "Algo/Decimation/edgeSelector.h"
 #include "Algo/Decimation/geometryApproximator.h"
+#include "Algo/Decimation/geometryPredictor.h"
+#include "Algo/Decimation/colorPerVertexApproximator.h"
 
-#include "Node.h"
+#include "Utils/quantization.h"
 
-namespace CGoGN {
+namespace CGoGN
+{
 
-namespace Algo {
+namespace Algo
+{
 
-namespace Surface {
+namespace Surface
+{
 
-namespace VDPMesh {
+namespace VDPMesh
+{
 
 template <typename PFP>
 class VDProgressiveMesh
 {
 public:
-    typedef typename PFP::MAP MAP;
-    typedef typename PFP::VEC3 VEC3;
-    typedef typename PFP::REAL REAL;
+	typedef typename PFP::MAP MAP ;
+	typedef typename PFP::VEC3 VEC3 ;
+	typedef typename PFP::REAL REAL ;
 
 private:
-    MAP& m_map;
-    VertexAttribute<typename PFP::VEC3>& positionsTable;
+	MAP& m_map ;
+	VertexAttribute<typename PFP::VEC3>& positionsTable ;
 
-    DartMarker& inactiveMarker;
-    SelectorUnmarked dartSelect;
+	DartMarker& inactiveMarker ;
+	SelectorUnmarked dartSelect ;
 
-    Algo::Surface::Decimation::EdgeSelector<PFP>* m_selector;
-    std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*> m_approximators;
-    
-    Algo::Surface::Decimation::Approximator<PFP, VEC3, EDGE>* m_positionsApproximator;
+	Algo::Surface::Decimation::EdgeSelector<PFP>* m_selector ;
+	std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*> m_approximators ;
+	std::vector<VSplit<PFP>*> m_splits ;
+	unsigned int m_cur ;
 
-    std::list<Node*> front; //Le front de l'arbre
+	Algo::Surface::Decimation::Approximator<PFP, VEC3, EDGE>* m_positionApproximator ;
+
+	bool m_initOk ;
 
 public:
-    VDProgressiveMesh(
-        MAP& map, DartMarker& inactive,
-        VertexAttribute<typename PFP::VEC3>& position
-    );
-    ~VDProgressiveMesh();
-    
-    /*
-     * Crée un noeud pour chacun des brins du maillage de départ (le plus fin)
-     */
-    void initialiseTree();
+	VDProgressiveMesh(
+		MAP& map, DartMarker& inactive,
+		VertexAttribute<typename PFP::VEC3>& position
+	) ;
+	~VDProgressiveMesh() ;
 
-    void createPM(unsigned int percentWantedVertices);
+	bool initOk() { return m_initOk ; }
 
-    Algo::Surface::Decimation::EdgeSelector<PFP>* selector() { return m_selector; }
-    std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>& approximators() { return m_approximators; }
+	void createPM(unsigned int percentWantedVertices) ;
 
-    void edgeCollapse(VSplit<PFP>* vs); 
-    void vertexSplit(VSplit<PFP>* vs);
+	std::vector<VSplit<PFP>*>& splits() { return m_splits ; }
+	Algo::Surface::Decimation::EdgeSelector<PFP>* selector() { return m_selector ; }
+	std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>& approximators() { return m_approximators ; }
 
-    void coarsen();
-    void refine();
-};
-}
-}
-}
-}
+	void edgeCollapse(VSplit<PFP>* vs) ;
+	void vertexSplit(VSplit<PFP>* vs) ;
 
-#include "VDPMesh.hpp"
+	void coarsen() ;
+	void refine() ;
+
+	void goToLevel(unsigned int goal) ;
+	unsigned int& currentLevel() { return m_cur ; }
+	unsigned int nbSplits() { return m_splits.size() ; }
+} ;
+
+} //namespace VDPMesh
+
+} //namespace Surface
+
+} //namespace Algo
+
+} //namespace CGoGN
+
+#include "PMesh.hpp"
 
 #endif
--- include/VDPMesh.hpp
+++ include/VDPMesh.hpp
@@ -1,3 +1,29 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2012, IGG Team, LSIIT, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#include "Algo/Geometry/localFrame.h"
+
 namespace CGoGN
 {
 
@@ -12,119 +38,120 @@ namespace VDPMesh
 
 template <typename PFP>
 VDProgressiveMesh<PFP>::VDProgressiveMesh(
-        MAP& map, DartMarker& inactive,
-        VertexAttribute<typename PFP::VEC3>& positions
-    ) :
-    m_map(map), positionsTable(positions), inactiveMarker(inactive), dartSelect(inactiveMarker)
+		MAP& map, DartMarker& inactive,
+		VertexAttribute<typename PFP::VEC3>& position
+	) :
+	m_map(map), positionsTable(position), inactiveMarker(inactive), dartSelect(inactiveMarker)
 {
-    CGoGNout << "  creating approximator.." << CGoGNflush;
+	CGoGNout << "  creating approximator .." << CGoGNflush ;
 
-    std::vector<VertexAttribute<typename PFP::VEC3>*> pos_v;
-    pos_v.push_back(&positionsTable);
+	std::vector<VertexAttribute< typename PFP::VEC3>* > pos_v ;
+	pos_v.push_back(&positionsTable) ;
+	m_approximators.push_back(new Algo::Surface::Decimation::Approximator_MidEdge<PFP>(m_map, pos_v)) ;
 
-    m_approximators.push_back(new Algo::Surface::Decimation::Approximator_MidEdge<PFP>(m_map, pos_v));
+	CGoGNout << "..done" << CGoGNendl ;
 
-    CGoGNout << "..done" << CGoGNendl;
+	CGoGNout << "  creating selector.." << CGoGNflush ;
+			
+    m_selector = new Algo::Surface::Decimation::EdgeSelector_Length<PFP>(m_map, positionsTable, m_approximators, dartSelect) ;
+	
+    CGoGNout << "..done" << CGoGNendl ;
 
-    CGoGNout << "  creating selector.." << CGoGNflush;
-    
-    m_selector = new Algo::Surface::Decimation::EdgeSelector_Length<PFP>(m_map, positionsTable, m_approximators, dartSelect);
-    
-    CGoGNout << "..done" << CGoGNendl;
+	m_initOk = true ;
 
-    CGoGNout << "  initializing selector.." << CGoGNflush;
+	CGoGNout << "  initializing approximators.." << CGoGNflush ;
 
-    m_selector->init();
+	for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
+	{
+		if(! (*it)->init())
+			m_initOk = false ;
+		if((*it)->getApproximatedAttributeName() == "position")
+			m_positionApproximator = reinterpret_cast<Algo::Surface::Decimation::Approximator<PFP, VEC3, EDGE>*>(*it) ;
+	}
 
-    CGoGNout << "..done" << CGoGNendl;
+	CGoGNout << "..done" << CGoGNendl ;
+
+	CGoGNout << "  initializing selector.." << CGoGNflush ;
+
+	m_initOk = m_selector->init() ;
+
+	CGoGNout << "..done" << CGoGNendl ;
 }
 
 template <typename PFP>
-VDProgressiveMesh<PFP>::~VDProgressiveMesh() 
+VDProgressiveMesh<PFP>::~VDProgressiveMesh()
 {
-    if(m_selector)
-        delete m_selector;
-    for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
-        delete (*it);
-}
-
-template <typename PFP>
-void VDProgressiveMesh<PFP>::initialiseTree()
-{
-    //Ajout d'un attribut de sommet de type Node
-    EdgeAttribute<Node*> noeud = m_map.template addAttribute<Node*, EGDE>("noeud");
-
-    AttributeContainer& container = m_map.template getAttributeContainer<EDGE>();
-    for(unsigned int i = container.begin(); i != container.end(); container.next(i)) 
-    {
-        noeud[i] = new Node();
-    }
+	for(unsigned int i = 0; i < m_splits.size(); ++i)
+		delete m_splits[i] ;
+	if(m_selector)
+		delete m_selector ;
+	for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
+		delete (*it) ;
 }
 
 template <typename PFP>
 void VDProgressiveMesh<PFP>::createPM(unsigned int percentWantedVertices)
 {
-    unsigned int nbVertices = m_map.template getNbOrbits<VERTEX>();
-    unsigned int nbWantedVertices = nbVertices * percentWantedVertices;
-	
-    CGoGNout << "  creating PM (" << nbVertices << " vertices).." << /* flush */ CGoGNendl ;
+	unsigned int nbVertices = m_map.template getNbOrbits<VERTEX>() ;
+	unsigned int nbWantedVertices = nbVertices * percentWantedVertices / 100 ;
+	CGoGNout << "  creating PM (" << nbVertices << " vertices).." << /* flush */ CGoGNendl ;
 
-    bool finished = false;
-    Dart d;
-    while(!finished) 
-    {
-        if(!m_selector->nextEdge(d))
-            break;
+	bool finished = false ;
+	Dart d ;
+	while(!finished)
+	{
+		if(!m_selector->nextEdge(d))
+			break ;
 
-        --nbVertices;
-        Dart d2 = m_map.phi2(m_map.phi_1(d));
-        Dart dd2 = m_map.phi2(m_map.phi_1(m_map.phi2(d)));
+		--nbVertices ;
+		Dart d2 = m_map.phi2(m_map.phi_1(d)) ;
+		Dart dd2 = m_map.phi2(m_map.phi_1(m_map.phi2(d))) ;
 
-        VSplit<PFP>* vs = new VSplit<PFP>(m_map, d, d2, dd2);
+		VSplit<PFP>* vs = new VSplit<PFP>(m_map, d, dd2, d2) ;	// create new VSplit node
+		m_splits.push_back(vs) ;								// and store it
 
-        EdgeAttribute<Node*> noeud = m_map.template getAttribute<Node*, EDGE>("noeud");
-        Node* node = noeud[d];  //On récupère le noeud associé à l'arête courante
+		for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
+		{
+			(*it)->approximate(d) ;					// compute approximated attributes with its associated detail
+			(*it)->saveApprox(d) ;
+		}
 
-        node->setVSplit(vs);    //On associe le vsplit construit au noeud de l'arête courante
+		m_selector->updateBeforeCollapse(d) ;		// update selector
 
-        for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
-        {
-            (*it)->approximate(d);
-            (*it)->saveApprox(d);
-        }
+		edgeCollapse(vs) ;							// collapse edge
 
-        m_selector->updateBeforeCollapse(d);
-        
-        edgeCollapse(VSplit<PFP>(m_map, d, d2, dd2));
+		unsigned int newV = m_map.template setOrbitEmbeddingOnNewCell<VERTEX>(d2) ;
+		unsigned int newE1 = m_map.template setOrbitEmbeddingOnNewCell<EDGE>(d2) ;
+		unsigned int newE2 = m_map.template setOrbitEmbeddingOnNewCell<EDGE>(dd2) ;
+		vs->setApproxV(newV) ;
+		vs->setApproxE1(newE1) ;
+		vs->setApproxE2(newE2) ;
 
-        unsigned int newV = m_map.template setOrbitEmbeddingOnNewCell<VERTEX>(d2);
-        vs->setApproxV(newV);
-		
-        for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
-			(*it)->affectApprox(d2);
+		for(typename std::vector<Algo::Surface::Decimation::ApproximatorGen<PFP>*>::iterator it = m_approximators.begin(); it != m_approximators.end(); ++it)
+			(*it)->affectApprox(d2);				// affect data to the resulting vertex
 
-        m_selector->updateAfterCollapse(d2, dd2);
+		m_selector->updateAfterCollapse(d2, dd2) ;	// update selector
 
-        if(nbVertices <= nbWantedVertices)
-            finished = true;
-    }
-    delete m_selector;
-    m_selector = NULL;
-	
-    CGoGNout << "..done (" << nbVertices << " vertices)" << CGoGNendl ;
+		if(nbVertices <= nbWantedVertices)
+			finished = true ;
+	}
+	delete m_selector ;
+	m_selector = NULL ;
 
+	m_cur = m_splits.size() ;
+	CGoGNout << "..done (" << nbVertices << " vertices)" << CGoGNendl ;
 }
 
 template <typename PFP>
 void VDProgressiveMesh<PFP>::edgeCollapse(VSplit<PFP>* vs)
 {
-    Dart d = vs->getEdge();
-	Dart dd = m_map.phi2(d);
-	
-    inactiveMarker.markOrbit<FACE>(d);
-	inactiveMarker.markOrbit<FACE>(dd);
+	Dart d = vs->getEdge() ;
+	Dart dd = m_map.phi2(d) ;
 
-	m_map.extractTrianglePair(d);
+	inactiveMarker.markOrbit<FACE>(d) ;
+	inactiveMarker.markOrbit<FACE>(dd) ;
+
+	m_map.extractTrianglePair(d) ;
 }
 
 template <typename PFP>
@@ -144,16 +171,72 @@ void VDProgressiveMesh<PFP>::vertexSplit(VSplit<PFP>* vs)
 template <typename PFP>
 void VDProgressiveMesh<PFP>::coarsen()
 {
+	if(m_cur == m_splits.size())
+		return ;
 
+	VSplit<PFP>* vs = m_splits[m_cur] ; // get the split node
+	++m_cur ;
+
+	// Dart d = vs->getEdge() ;
+	// Dart dd = m_map.phi2(d) ;		// get some darts
+	Dart d2 = vs->getLeftEdge() ;
+	Dart dd2 = vs->getLeftEdge() ;
+
+	edgeCollapse(vs) ;	// collapse edge
+
+	m_map.template setOrbitEmbedding<VERTEX>(d2, vs->getApproxV()) ;
+	m_map.template setOrbitEmbedding<EDGE>(d2, vs->getApproxE1()) ;
+	m_map.template setOrbitEmbedding<EDGE>(dd2, vs->getApproxE2()) ;
 }
 
 template <typename PFP>
 void VDProgressiveMesh<PFP>::refine()
 {
-    
+	if(m_cur == 0)
+		return ;
+
+	--m_cur ;
+	VSplit<PFP>* vs = m_splits[m_cur] ; // get the split node
+
+	Dart d = vs->getEdge() ;
+	Dart dd = m_map.phi2(d) ; 		// get some darts
+	Dart dd2 = vs->getRightEdge() ;
+	Dart d2 = vs->getLeftEdge() ;
+	Dart d1 = m_map.phi2(d2) ;
+	Dart dd1 = m_map.phi2(dd2) ;
+
+	unsigned int v1 = m_map.template getEmbedding<VERTEX>(d) ;				// get the embedding
+	unsigned int v2 = m_map.template getEmbedding<VERTEX>(dd) ;			// of the new vertices
+	unsigned int e1 = m_map.template getEmbedding<EDGE>(m_map.phi1(d)) ;
+	unsigned int e2 = m_map.template getEmbedding<EDGE>(m_map.phi_1(d)) ;	// and new edges
+	unsigned int e3 = m_map.template getEmbedding<EDGE>(m_map.phi1(dd)) ;
+	unsigned int e4 = m_map.template getEmbedding<EDGE>(m_map.phi_1(dd)) ;
+
+	vertexSplit(vs) ; // split vertex
+
+	m_map.template setOrbitEmbedding<VERTEX>(d, v1) ;		// embed the
+	m_map.template setOrbitEmbedding<VERTEX>(dd, v2) ;	// new vertices
+	m_map.template setOrbitEmbedding<EDGE>(d1, e1) ;
+	m_map.template setOrbitEmbedding<EDGE>(d2, e2) ;		// and new edges
+	m_map.template setOrbitEmbedding<EDGE>(dd1, e3) ;
+	m_map.template setOrbitEmbedding<EDGE>(dd2, e4) ;
 }
 
+template <typename PFP>
+void VDProgressiveMesh<PFP>::goToLevel(unsigned int l)
+{
+	if(l == m_cur || l > m_splits.size() || l < 0)
+		return ;
+
+	if(l > m_cur)
+		while(m_cur != l)
+			coarsen() ;
+	else
+		while(m_cur != l)
+			refine() ;
 }
-}
-}
-}
+
+} // namespace VDPMesh
+} // namespace Surface
+} // namespace Algo
+} // namespace CGoGN
